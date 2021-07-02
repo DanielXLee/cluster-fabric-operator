7d58c256ef7805a632e1b13d9e055e1c6c614758
Xin Li <lixin8611@gmail.com>
2021-07-02 10:14:52 +0800

    Update api

--- api/v1alpha1/fabric_types.go
+++ api/v1alpha1/fabric_types.go
@@ -40,68 +40,68 @@ type FabricSpec struct {
 	CloudPrepareConfig `json:"cloudPrepareConfig,omitempty"`
 }
 
-type BrokerConfig struct {
-	IpsecSubmFile               string        `json:"ipsecSubmFile              ,omitempty"`
-	GlobalnetEnable             bool          `json:"globalnetEnable            ,omitempty"`
-	GlobalnetCIDRRange          string        `json:"globalnetCIDRRange         ,omitempty"`
-	DefaultGlobalnetClusterSize uint          `json:"defaultGlobalnetClusterSize,omitempty"`
-	ServiceDiscoveryEnabled     bool          `json:"serviceDiscoveryEnabled    ,omitempty"`
-	ComponentArr                []string      `json:"componentArr               ,omitempty"`
-	GlobalCIDRConfigMap         *v1.ConfigMap `json:"globalCIDRConfigMap           ,omitempty"`
-	DefaultCustomDomains        []string      `json:"defaultCustomDomains       ,omitempty"`
-}
-
-type JoinConfig struct {
-	ClusterID     string `json:"clusterID                    ,omitempty"`
-	ServiceCIDR   string `json:"serviceCIDR                  ,omitempty"`
-	ClusterCIDR   string `json:"clusterCIDR                  ,omitempty"`
-	GlobalnetCIDR string `json:"globalnetCIDR                ,omitempty"`
-	Repository    string `json:"repository                   ,omitempty"`
-	ImageVersion  string `json:"imageVersion                 ,omitempty"`
-	// NattPort represents IPsec NAT-T port (default 4500).
-	// +optional
-	// +kubebuilder:default=4500
-
-	NattPort int `json:"nattPort                     ,omitempty"`
-	// IkePort represents IPsec IKE port (default 500).
-	// +optional
-	// +kubebuilder:default=500
-	IkePort                       int        `json:"ikePort                      ,omitempty"`
-	PreferredServer               bool       `json:"preferredServer              ,omitempty"`
-	ForceUDPEncaps                bool       `json:"forceUDPEncaps               ,omitempty"`
-	NatTraversal                  bool       `json:"natTraversal                 ,omitempty"`
-	GlobalnetEnabled              bool       `json:"globalnetEnabled             ,omitempty"`
-	IpsecDebug                    bool       `json:"ipsecDebug                   ,omitempty"`
-	SubmarinerDebug               bool       `json:"submarinerDebug              ,omitempty"`
-	LabelGateway                  bool       `json:"labelGateway                 ,omitempty"`
-	LoadBalancerEnabled           bool       `json:"loadBalancerEnabled          ,omitempty"`
-	CableDriver                   string     `json:"cableDriver                  ,omitempty"`
-	ClientToken                   *v1.Secret `json:"clientToken                  ,omitempty"`
-	GlobalnetClusterSize          uint       `json:"globalnetClusterSize         ,omitempty"`
-	CustomDomains                 []string   `json:"customDomains                ,omitempty"`
-	ImageOverrideArr              []string   `json:"imageOverrideArr             ,omitempty"`
-	HealthCheckEnable             bool       `json:"healthCheckEnable            ,omitempty"`
-	HealthCheckInterval           uint64     `json:"healthCheckInterval          ,omitempty"`
-	HealthCheckMaxPacketLossCount uint64     `json:"healthCheckMaxPacketLossCount,omitempty"`
-	CorednsCustomConfigMap        string     `json:"corednsCustomConfigMap         ,omitempty"`
-}
-
 // FabricStatus defines the observed state of Fabric
 type FabricStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 }
 
+type BrokerConfig struct {
+	IpsecSubmFile               string        `json:"ipsecSubmFile,omitempty"`
+	GlobalnetEnable             bool          `json:"globalnetEnable,omitempty"`
+	GlobalnetCIDRRange          string        `json:"globalnetCIDRRange,omitempty"`
+	DefaultGlobalnetClusterSize uint          `json:"defaultGlobalnetClusterSize,omitempty"`
+	ServiceDiscoveryEnabled     bool          `json:"serviceDiscoveryEnabled,omitempty"`
+	ComponentArr                []string      `json:"componentArr,omitempty"`
+	GlobalCIDRConfigMap         *v1.ConfigMap `json:"globalCIDRConfigMap,omitempty"`
+	DefaultCustomDomains        []string      `json:"defaultCustomDomains,omitempty"`
+}
+
+type JoinConfig struct {
+	ClusterID     string `json:"clusterID,omitempty"`
+	ServiceCIDR   string `json:"serviceCIDR,omitempty"`
+	ClusterCIDR   string `json:"clusterCIDR,omitempty"`
+	GlobalnetCIDR string `json:"globalnetCIDR,omitempty"`
+	Repository    string `json:"repository,omitempty"`
+	ImageVersion  string `json:"imageVersion,omitempty"`
+	// NattPort represents IPsec NAT-T port (default 4500).
+	// +optional
+	// +kubebuilder:default=4500
+
+	NattPort int `json:"nattPort,omitempty"`
+	// IkePort represents IPsec IKE port (default 500).
+	// +optional
+	// +kubebuilder:default=500
+	IkePort                       int        `json:"ikePort,omitempty"`
+	PreferredServer               bool       `json:"preferredServer,omitempty"`
+	ForceUDPEncaps                bool       `json:"forceUDPEncaps,omitempty"`
+	NatTraversal                  bool       `json:"natTraversal,omitempty"`
+	GlobalnetEnabled              bool       `json:"globalnetEnabled,omitempty"`
+	IpsecDebug                    bool       `json:"ipsecDebug,omitempty"`
+	SubmarinerDebug               bool       `json:"submarinerDebug,omitempty"`
+	LabelGateway                  bool       `json:"labelGateway,omitempty"`
+	LoadBalancerEnabled           bool       `json:"loadBalancerEnabled,omitempty"`
+	CableDriver                   string     `json:"cableDriver,omitempty"`
+	ClientToken                   *v1.Secret `json:"clientToken,omitempty"`
+	GlobalnetClusterSize          uint       `json:"globalnetClusterSize,omitempty"`
+	CustomDomains                 []string   `json:"customDomains,omitempty"`
+	ImageOverrideArr              []string   `json:"imageOverrideArr,omitempty"`
+	HealthCheckEnable             bool       `json:"healthCheckEnable,omitempty"`
+	HealthCheckInterval           uint64     `json:"healthCheckInterval,omitempty"`
+	HealthCheckMaxPacketLossCount uint64     `json:"healthCheckMaxPacketLossCount,omitempty"`
+	CorednsCustomConfigMap        string     `json:"corednsCustomConfigMap,omitempty"`
+}
+
 type CloudPrepareConfig struct {
 	// Vendor represents cloud vendor, example: aws, gcp
 	Vendor string `json:"vendor,omitempty"`
-	AWS    `json:"aws,omitempty"`
+	// AWS represents aws cloud prepare setup
+	AWS `json:"aws,omitempty"`
 }
 
 type AWS struct {
 	// AWS credentials configuration file (default "/root/.aws/credentials")
 	Credentials string `json:"credentials,omitempty"`
-
 	// GatewayInstance represents type of gateways instance machine (default "m5n.large")
 	// +optional
 	// +kubebuilder:default=m5n.large
@@ -118,16 +118,26 @@ type AWS struct {
 	Profile string `json:"profile,omitempty"`
 	// AWS regio
 	Region string `json:"region,omitempty"`
+	// CommonPrepareConfig represents common setup for cloud vendor
+	CommonPrepareConfig `json:"commonConfig,omitempty"`
 }
 
-type GlobalPrepareConfig struct {
+type CommonPrepareConfig struct {
 	// Metrics port (default 8080)
+	// +optional
+	// +kubebuilder:default=8080
 	MetricsPort uint16 `json:"metricsPort,omitempty"`
 	// NAT discovery port (default 4490)
+	// +optional
+	// +kubebuilder:default=4490
 	NatDiscoveryPort uint16 `json:"natDiscoveryPort,omitempty"`
 	// IPSec NAT traversal port (default 4500)
+	// +optional
+	// +kubebuilder:default=4500
 	NattPort uint16 `json:"nattPort,omitempty"`
 	// Internal VXLAN port (default 4800)
+	// +optional
+	// +kubebuilder:default=4800
 	VxlanPort uint16 `json:"vxlanPort,omitempty"`
 }
 
